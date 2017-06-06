b69a729e1d631f57feff08a5ca6e6decb36b180b
Benjamin Beguin <beguin.ben@gmail.com>
2017-06-06 15:41:51 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,53 @@
 
 
 1.times do
-  u = User.create(email: "martinziserman@gmail.com", password: "aaaaaaaa",
-    password_confirmation: "aaaaaaaa", first_name: "martin", last_name: "ziserman",
-    avatar: "www.google.com", pseudo: "zarmilf", channel: "zarmilf_is_live")
+  u = User.create(
+    email: "martinziserman@gmail.com",
+    password: "password",
+    password_confirmation: "password",
+    first_name: "martin",
+    last_name: "ziserman",
+    avatar: "www.google.com",
+    pseudo: "zarmilf",
+    channel: "ogaminglol"
+  )
   u.streams.create
-  u = User.create(email: "ben@gmail.com", password: "aaaaaaaa",
-    password_confirmation: "aaaaaaaa")
-  u.streams.create
+
+
+  u = User.create(
+    email: "ben@gmail.com",
+    password: "password",
+    password_confirmation: "password",
+    first_name: "martin",
+    last_name: "ziserman",
+    avatar: "www.google.com",
+    pseudo: "zarmilf",
+    channel: "ogaminglol",
+    live: true
+  )
+
+  u = User.create(
+    email: "pauline@gmail.com",
+    password: "password",
+    password_confirmation: "password",
+    first_name: "Pauline",
+    last_name: "ziserman",
+    avatar: "www.google.com",
+    pseudo: "zarmilf",
+    channel: "ogaminglol",
+    live: true
+  )
+
+  u = User.create(
+    email: "matthieu@gmail.com",
+    password: "password",
+    password_confirmation: "password",
+    first_name: "Matthieu",
+    last_name: "Taveneau",
+    avatar: "www.google.com",
+    pseudo: "zarmilf",
+    channel: "ogaminglol",
+    live: true
+  )
+  u.streams.create()
 end
