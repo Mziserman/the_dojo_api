7d05282f740b30aa44f3b3e0628fbafa246f6921
Ziserman <martinziserman@gmail.com>
2017-06-06 11:24:52 +0200

    add migration

---
+++ db/migrate/20170606084109_add_live_to_user.rb
@@ -0,0 +1,6 @@
+class AddLiveToUser < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :live, :boolean
+    add_column :users, :channel, :string
+  end
+end
---
+++ db/migrate/20170606084450_drop_table_streams.rb
@@ -0,0 +1,5 @@
+class DropTableStreams < ActiveRecord::Migration[5.0]
+  def change
+    drop_table :streams
+  end
+end
