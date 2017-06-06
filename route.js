b58f28a7e02b72cde0fb0d247469fd0df80e23df
Benjamin Beguin <beguin.ben@gmail.com>
2017-06-06 17:31:08 +0200

    route file

---
+++ route.js
@@ -0,0 +1,24 @@
+/*
+
+-------------------
+HOME
+
+Get all category
+Get all category with a leat one live
+Get the best stream ( most viewer ) LIMIT 1
+
+------------------------
+Parcourir
+
+get all the streams ( user with streams online )
+get all the stream by subCategory in a Category
+
+----------------------
+Single stream
+
+Get all the info about the stream ( twitch data + chat data + file_path data )........ ( + VOD ? )
+
+
+
+
+*/
