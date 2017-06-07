f377a2955a1ab224bf594d4f8bc5b1296508fa7a
Ziserman <martinziserman@gmail.com>
2017-06-07 10:41:10 +0200

    twitch migration

---
+++ db/migrate/20170606221518_add_twitch_to_streams.rb
@@ -0,0 +1,9 @@
+class AddTwitchToStreams < ActiveRecord::Migration[5.0]
+  def change
+    add_column :streams, :twitch_stream_id, :integer, limit: 8
+    add_column :streams, :twitch_created_at, :datetime
+    add_column :streams, :viewers, :integer, default: 0
+    add_column :streams, :live, :boolean, default: false
+    add_column :streams, :max_viewers, :integer, default: 0
+  end
+end
