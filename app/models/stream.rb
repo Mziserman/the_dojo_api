84d7c501a6ab6aef8fbea18423cc4783f06c2870
Ziserman <martinziserman@gmail.com>
2017-06-07 11:54:35 +0200

    twitch in seeds

--- app/models/stream.rb
+++ app/models/stream.rb
@@ -2,6 +2,19 @@ class Stream < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :sub_categories
   belongs_to :category
+  before_save :bind_twitch
+
+  def bind_twitch
+    response = HTTParty.get('https://api.twitch.tv/kraken/streams/' +
+      self.user.channel + '?client_id=t0wqumd7sh0dbsyxt2mli4r93jsxhn')
+
+    self.twitch_stream_id = response["stream"]["_id"]
+    self.twitch_created_at = DateTime.parse(response["stream"]["created_at"]),
+    self.name = response["stream"]["channel"]["status"],
+    self.viewers = response["stream"]["viewers"],
+    self.max_viewers = response["stream"]["viewers"]
+  end
+
 
   def uptime
     Time.now - self.twitch_created_at
