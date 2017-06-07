350b3e5ce86e93c20a644cebb92f62b2c1103134
Thomas Victoria <thomas@MacBook-Pro-de-Thomas.local>
2017-06-07 15:06:41 +0200

    fix(sub_category): slug fix

--- app/models/sub_category.rb
+++ app/models/sub_category.rb
@@ -2,9 +2,9 @@ class SubCategory < ApplicationRecord
   has_and_belongs_to_many :streams
   belongs_to :category
 
-  before_save :slug
+  before_save :slug_attr
 
-  def slug
+  def slug_attr
     self.slug = self.name.slugify
   end
 
