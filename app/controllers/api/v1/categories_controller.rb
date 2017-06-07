577bac44fdfd96f6e0f4950642ad045ed9405f61
Thomas Victoria <thomas@MacBook-Pro-de-Thomas.local>
2017-06-07 10:56:33 +0200

    feat(category): category route

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,3 +1,12 @@
-class Api::V1::SessionsController < ApplicationController
-  
+class Api::V1::CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.all
+
+    render 'index.json'
+  end
+
+  def show
+  end
+
 end
