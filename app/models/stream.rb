100244f5c9a2dd83360a6b8d50c9f3a1e7926269
Ziserman <martinziserman@gmail.com>
2017-06-12 18:48:57 +0200

    yolo

--- app/models/stream.rb
+++ app/models/stream.rb
@@ -30,7 +30,7 @@ class Stream < ApplicationRecord
       throw :abort
     end
 
-    self.user.streams.live.update_all(live: false)
+    # self.user.streams.live.update_all(live: false)
 
     response = HTTParty.get('https://api.twitch.tv/kraken/streams/' +
       self.user.channel + '?client_id=' + Settings.twitch.client_id)
@@ -53,7 +53,7 @@ class Stream < ApplicationRecord
     if response["stream"].nil?
       self.live = false
     else
-      self.user.streams.live.update_all(live: false)
+      # self.user.streams.live.update_all(live: false)
       self.live = true
       self.viewers = response["stream"]["viewers"]
       self.thumbnail = response["stream"]["preview"]["large"]
