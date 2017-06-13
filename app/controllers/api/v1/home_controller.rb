ab7975e24cdf33d3d30886bd4260d738a7c7bc69
Thomas Victoria <thomas@MacBook-Pro-de-Thomas.local>
2017-06-13 14:54:06 +0200

    fix(home): get 500

--- app/controllers/api/v1/home_controller.rb
+++ app/controllers/api/v1/home_controller.rb
@@ -1,9 +1,8 @@
 class Api::V1::HomeController < ApplicationController
   def index
     @categories = Category.all
-    @main_stream = Stream.first
+    @main_stream = Stream.last
     @main_stream.update_twitch_data
-
 
     render 'index.json'
   end
